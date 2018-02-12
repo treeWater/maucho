80dfe373b0dd51a39a3f3eba589c1a8d0e25daeb
Thomas Chang <tianlong8bu@gmail.com>
2018-02-12 23:09:51 +0800

    initial commit

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,19 @@
+
+var gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+cssImport = require('postcss-import'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba'),
+autoprefixer = require('autoprefixer');
+
+gulp.task('styles', function() {
+	return gulp.src('./app/assets/styles/style.css')
+	.pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
+	.on('error', function(errorInfo) {
+		console.log(errorInfo.toString());
+		this.emit('end');
+	})
+	.pipe(gulp.dest('./app/temp/styles'));
+});
